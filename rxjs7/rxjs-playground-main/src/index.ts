4846ed021c8572513ee644dabc32f73f3de79ffe
unlimit1984 <unlimit_rmc@mail.ru>
2022-11-22 17:25:26 +0100

    [rxjs] 09 Add  more code

--- rxjs7/rxjs-playground-main/src/index.ts
+++ rxjs7/rxjs-playground-main/src/index.ts
@@ -29,20 +29,45 @@
 //   error: err => console.log('Error when saving:', err.message)
 // });
 
+// ---------------------------------------------------------------------------
+
 //09
+// import { Observable, Subscriber } from 'rxjs';
+
+// //Observable
+// const observable$ = new Observable(subscriber => {
+//   subscriber.next('Alice');
+//   subscriber.next('Bob');
+// });
+
+// //Observer
+// const observer = {
+//   next: value => console.log(value)
+// };
+
+// //Subscription
+// observable$.subscribe(observer);
+
+// ---------------------------------------------------------------------------
+
+//10
 import { Observable, Subscriber } from 'rxjs';
 
-//Observable
-const observable$ = new Observable(subscriber => {
+const observable$ = new Observable<string>(subscriber => {
+  console.log('Observable executed');
   subscriber.next('Alice');
-  subscriber.next('Bob');
+  subscriber.next('Ben');
+  subscriber.next('Charlie');
 });
 
-//Observer
+//observable$.subscribe(); //emit values without any wired subscriber
+
 const observer = {
   next: value => console.log(value)
 };
 
-//Subscription
-observable$.subscribe(observer);
+//OPTION 1
+// observable$.subscribe(observer);
 
+//OPTION 2
+observable$.subscribe(value => console.log(value));
