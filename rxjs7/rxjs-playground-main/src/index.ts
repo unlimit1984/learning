31e9dd329a1dd45f1d241118bdad9787c8798e4d
unlimit1984 <unlimit_rmc@mail.ru>
2022-11-22 19:43:33 +0100

    [rxjs] 22 Add empty observable

--- rxjs7/rxjs-playground-main/src/index.ts
+++ rxjs7/rxjs-playground-main/src/index.ts
@@ -53,21 +53,48 @@
 //10
 import { Observable, Subscriber } from 'rxjs';
 
-const observable$ = new Observable<string>(subscriber => {
-  console.log('Observable executed');
-  subscriber.next('Alice');
-  subscriber.next('Ben');
-  subscriber.next('Charlie');
-});
+// const observable$ = new Observable<string>(subscriber => {
+//   console.log('Observable executed');
+//   subscriber.next('Alice');
+//   setTimeout(() => subscriber.next('Ben'), 2000);
+//   setTimeout(() => subscriber.next('Charlie'), 4000);
+// });
 
 //observable$.subscribe(); //emit values without any wired subscriber
 
-const observer = {
-  next: value => console.log(value)
-};
+// const observer = {
+//   next: value => console.log(value)
+// };
 
 //OPTION 1
 // observable$.subscribe(observer);
 
 //OPTION 2
-observable$.subscribe(value => console.log(value));
+// observable$.subscribe(value => console.log(value));
+
+//const subscription = observable$.subscribe(value => console.log(value));
+
+// setTimeout(() => {
+//   console.log('Unsubscribe');
+//   subscription.unsubscribe();
+// }, 3000);
+
+// console.log('Subscription 1 starts');
+// observable$.subscribe(value => console.log('Subscription 1', value));
+//
+// setTimeout(() => {
+//   console.log('Subscription 2 starts');
+//   observable$.subscribe(value => console.log('Subscription 2', value));
+// }, 1000);
+
+//22 - Empty Observable
+
+const observable = new Observable(subscriber => {
+  console.log('Observable executed');
+});
+
+console.log('Before subscribe');
+observable.subscribe({
+  next: value => console.log(value)
+});
+console.log('After subscribe');
