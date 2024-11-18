f57cd8fe81ffc3d537a453d8097847d73d27c7a0
unlimit1984 <unlimit_rmc@mail.ru>
2024-11-18 15:45:05 +0100

    [angular signals] Prepare again

--- angular/angular-signals-course/src/app/home/home.component.ts
+++ angular/angular-signals-course/src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { afterNextRender, Component, computed, effect, EffectRef, inject, Injector, signal } from '@angular/core';
+import { Component, computed, effect, inject, Injector, signal } from '@angular/core';
 import { CoursesService } from '../services/courses.service';
 import { Course, sortCoursesBySeqNo } from '../models/course.model';
 import { MatTab, MatTabGroup } from '@angular/material/tabs';
@@ -8,10 +8,6 @@ import { MessagesService } from '../messages/messages.service';
 import { catchError, from, throwError } from 'rxjs';
 import { toObservable, toSignal, outputToObservable, outputFromObservable } from '@angular/core/rxjs-interop';
 
-type Counter = {
-  value: number;
-};
-
 @Component({
   selector: 'home',
   standalone: true,
@@ -19,76 +15,4 @@ type Counter = {
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
-export class HomeComponent {
-  counter = signal(0);
-  // counter = signal(0).asReadonly();
-  // counter = signal<Counter>({
-  //   value: 0
-  // });
-
-  tenXCounter = computed(() => {
-    const val = this.counter();
-    return val * 10;
-  });
-
-  hundredXCounter = computed(() => {
-    const val = this.tenXCounter();
-    return val * 10;
-  });
-
-  values = signal<number[]>([0]);
-
-  // injector = inject(Injector);
-
-  effectRef: EffectRef | null = null;
-
-  constructor() {
-    this.effectRef = effect(
-      // () => {
-      (onCleanup) => {
-        const counter = this.counter();
-        const timeout = setTimeout(() => {
-          console.log(`counter value: ${counter}`);
-        }, 1000);
-
-        //The cleanup function makes it possible to "cancel" any work that the
-        // previous effect run might have started
-        onCleanup(() => {
-          console.log(`Calling cleanup`);
-          clearTimeout(timeout);
-        });
-        // console.log(`counter value: ${this.counter()}`);
-        // this.increment();
-      },
-      {
-        // allowSignalWrites: false
-      }
-    );
-
-    //In case of non-standard usage provide injector for proper cleanup
-    // afterNextRender(()=>{
-    //   effect(() => {
-    //     console.log(`counter value: ${this.counter()}`);
-    //   },{
-    //     injector: this.injector
-    //   });
-    // })
-  }
-
-  increment() {
-    // this.counter.set(this.counter() + 1);
-    this.counter.update((counter) => counter + 1);
-    // this.counter.update((counter) => ({
-    //   ...counter,
-    //   value: counter.value + 1
-    // }));
-  }
-
-  append() {
-    this.values.update((values) => [...values, values[values.length - 1] + 1]);
-  }
-
-  cleanup() {
-    this.effectRef?.destroy();
-  }
-}
+export class HomeComponent {}
