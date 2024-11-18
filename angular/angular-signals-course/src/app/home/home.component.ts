7cb19853ba17c95062a140f47dc8b0ec49f7fcc6
unlimit1984 <unlimit_rmc@mail.ru>
2024-11-18 11:51:58 +0100

    [angular signals] 09 Using Arrays
    - Add prettier

--- angular/angular-signals-course/src/app/home/home.component.ts
+++ angular/angular-signals-course/src/app/home/home.component.ts
@@ -1,11 +1,4 @@
-import {
-  Component,
-  computed,
-  effect,
-  inject,
-  Injector,
-  signal,
-} from '@angular/core';
+import { Component, computed, effect, inject, Injector, signal } from '@angular/core';
 import { CoursesService } from '../services/courses.service';
 import { Course, sortCoursesBySeqNo } from '../models/course.model';
 import { MatTab, MatTabGroup } from '@angular/material/tabs';
@@ -13,12 +6,7 @@ import { CoursesCardListComponent } from '../courses-card-list/courses-card-list
 import { MatDialog } from '@angular/material/dialog';
 import { MessagesService } from '../messages/messages.service';
 import { catchError, from, throwError } from 'rxjs';
-import {
-  toObservable,
-  toSignal,
-  outputToObservable,
-  outputFromObservable,
-} from '@angular/core/rxjs-interop';
+import { toObservable, toSignal, outputToObservable, outputFromObservable } from '@angular/core/rxjs-interop';
 
 type Counter = {
   value: number;
@@ -29,21 +17,27 @@ type Counter = {
   standalone: true,
   imports: [MatTabGroup, MatTab, CoursesCardListComponent],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss',
+  styleUrl: './home.component.scss'
 })
 export class HomeComponent {
   // counter = signal(0);
   // counter = signal(0).asReadonly();
   counter = signal<Counter>({
-    value: 0,
+    value: 0
   });
+
+  values = signal<number[]>([0]);
 
   increment() {
     // this.counter.set(this.counter() + 1);
     // this.counter.update((counter) => counter + 1);
     this.counter.update((counter) => ({
       ...counter,
-      value: counter.value + 1,
+      value: counter.value + 1
     }));
+  }
+
+  append() {
+    this.values.update((values) => [...values, values[values.length - 1] + 1]);
   }
 }
