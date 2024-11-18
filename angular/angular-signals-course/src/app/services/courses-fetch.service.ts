07354ff86af0e57ef299e39560830ff2e1af6b74
unlimit1984 <unlimit_rmc@mail.ru>
2024-11-18 16:36:55 +0100

    [angular signals] 18_1 Two different options

--- angular/angular-signals-course/src/app/services/courses-fetch.service.ts
+++ angular/angular-signals-course/src/app/services/courses-fetch.service.ts
@@ -1,14 +1,22 @@
-import {Injectable} from "@angular/core";
-import {environment} from "../../environments/environment";
-import {Course} from "../models/course.model";
-
+import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environment';
+import { Course } from '../models/course.model';
 
 @Injectable({
-  providedIn: "root"
+  providedIn: 'root'
 })
 export class CoursesServiceWithFetch {
-
   env = environment;
 
+  async loadAllCourses(): Promise<Course[]> {
+    const response = await fetch(`${this.env.apiRoot}/api/courses`);
+    const payload = await response.json();
+    // return payload.courses as Course[];
 
+    // return Promise.resolve(payload.courses);
+    // So the async await syntax is going to inspect the return type.
+    // If it's a promise like we are doing here, it's going to use the promise directly.
+    // If it's a primitive type it's going to wrap it in a promise.
+    return payload.courses;
+  }
 }
