ee8396272355d94f1572aa5dbf5e46a71fcf3867
unlimit1984 <unlimit_rmc@mail.ru>
2024-11-18 09:24:20 +0100

    [angular signals] Init

---
+++ angular/angular-signals-course/src/app/courses-card-list/courses-card-list.component.ts
@@ -0,0 +1,17 @@
+import {Component, inject, input, output} from '@angular/core';
+import {RouterLink} from "@angular/router";
+import {Course} from "../models/course.model";
+import {MatDialog} from "@angular/material/dialog";
+
+@Component({
+  selector: 'courses-card-list',
+  standalone: true,
+  imports: [
+    RouterLink
+  ],
+  templateUrl: './courses-card-list.component.html',
+  styleUrl: './courses-card-list.component.scss'
+})
+export class CoursesCardListComponent {
+
+}
---
+++ angular/angular-signals-course/src/app/messages/messages.component.ts
@@ -0,0 +1,17 @@
+import {Component, inject} from "@angular/core";
+import {MessagesService} from "./messages.service";
+import {NgClass} from "@angular/common";
+
+@Component({
+  selector: 'messages',
+  templateUrl: './messages.component.html',
+  styleUrls: ['./messages.component.scss'],
+  imports: [
+    NgClass
+  ],
+  standalone: true
+})
+export class MessagesComponent {
+
+
+}
