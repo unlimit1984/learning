9e495ae8e076d8fa95a52320c66e97c5efe0c272
unlimit1984 <unlimit_rmc@mail.ru>
2024-11-24 14:52:43 +0100

    [angular signals] 23 required input signal and optional

--- angular/angular-signals-course/src/app/courses-card-list/courses-card-list.component.ts
+++ angular/angular-signals-course/src/app/courses-card-list/courses-card-list.component.ts
@@ -1,17 +1,15 @@
-import {Component, inject, input, output} from '@angular/core';
-import {RouterLink} from "@angular/router";
-import {Course} from "../models/course.model";
-import {MatDialog} from "@angular/material/dialog";
+import { Component, inject, input, output } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { Course } from '../models/course.model';
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'courses-card-list',
   standalone: true,
-  imports: [
-    RouterLink
-  ],
+  imports: [RouterLink],
   templateUrl: './courses-card-list.component.html',
   styleUrl: './courses-card-list.component.scss'
 })
 export class CoursesCardListComponent {
-
+  courses = input.required  <Course[]>();
 }
