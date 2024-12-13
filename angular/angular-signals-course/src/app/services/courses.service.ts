2b6608ff469c8bbfba70232763b68e0d90fa2b29
unlimit1984 <unlimit_rmc@mail.ru>
2024-12-13 21:31:21 +0100

    [angular signals] 35 Use HttpContext for loading

--- angular/angular-signals-course/src/app/services/courses.service.ts
+++ angular/angular-signals-course/src/app/services/courses.service.ts
@@ -1,9 +1,10 @@
 import { inject, Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpContext } from '@angular/common/http';
 import { environment } from '../../environments/environment';
 import { firstValueFrom } from 'rxjs';
 import { Course } from '../models/course.model';
 import { GetCoursesResponse } from '../models/get-courses.response';
+import { SkipLoading } from '../loading/skip-loading.component';
 
 @Injectable({
   providedIn: 'root'
@@ -22,7 +23,12 @@ export class CoursesService {
     // it's going to be wrapped automatically in a promise by the async syntax.
     // return [];
 
-    const courses$ = this.http.get<GetCoursesResponse>(`${this.env.apiRoot}/courses`);
+    const courses$ = this.http.get<GetCoursesResponse>(
+      `${this.env.apiRoot}/courses`,
+      {
+        context: new HttpContext().set(SkipLoading, true)
+      }
+    );
 
     //deprecated
     //courses$.toPromise()
@@ -31,18 +37,27 @@ export class CoursesService {
   }
 
   async createCourse(course: Partial<Course>): Promise<Course> {
-    const course$ = this.http.post<Course>(`${this.env.apiRoot}/courses`, course);
+    const course$ = this.http.post<Course>(
+      `${this.env.apiRoot}/courses`,
+      course
+    );
     // return await firstValueFrom(course$); //redundant
     return firstValueFrom(course$);
   }
 
-  async saveCourse(courseId: string, changes: Partial<Course>): Promise<Course> {
-    const course$ = this.http.put<Course>(`${this.env.apiRoot}/courses/${courseId}`, changes);
+  async saveCourse(
+    courseId: string,
+    changes: Partial<Course>
+  ): Promise<Course> {
+    const course$ = this.http.put<Course>(
+      `${this.env.apiRoot}/courses/${courseId}`,
+      changes
+    );
     // return await firstValueFrom(course$); //redundant
     return firstValueFrom(course$);
   }
 
-  async deleteCourse(courseId: string){
+  async deleteCourse(courseId: string) {
     const delete$ = this.http.delete(`${this.env.apiRoot}/courses/${courseId}`);
     // return await firstValueFrom(delete$); //redundant
     return firstValueFrom(delete$);
