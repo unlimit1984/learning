d563bd2f9d7b222cb2c1072bc52554bca89e8bcb
unlimit1984 <unlimit_rmc@mail.ru>
2024-12-13 16:41:51 +0100

    [angular signals] 27 Edit course form

--- angular/angular-signals-course/src/app/edit-course-dialog/edit-course-dialog.component.ts
+++ angular/angular-signals-course/src/app/edit-course-dialog/edit-course-dialog.component.ts
@@ -4,7 +4,7 @@ import { Course } from '../models/course.model';
 import { EditCourseDialogData } from './edit-course-dialog.data.model';
 import { CoursesService } from '../services/courses.service';
 import { LoadingIndicatorComponent } from '../loading/loading.component';
-import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { CourseCategoryComboboxComponent } from '../course-category-combobox/course-category-combobox.component';
 import { CourseCategory } from '../models/course-category.model';
 import { firstValueFrom } from 'rxjs';
@@ -18,6 +18,23 @@ import { firstValueFrom } from 'rxjs';
 })
 export class EditCourseDialogComponent {
   dialogRef = inject(MatDialogRef);
+  data: EditCourseDialogData = inject(MAT_DIALOG_DATA);
+  fb = inject(FormBuilder);
+  form = this.fb.group({
+    title: [''],
+    longDescription: [''],
+    category: [''],
+    iconUrl: ['']
+  });
+
+  constructor() {
+    this.form.patchValue({
+      title: this.data?.course?.title,
+      longDescription: this.data?.course?.longDescription,
+      category: this.data?.course?.category,
+      iconUrl: this.data?.course?.iconUrl
+    });
+  }
 
   onClose() {
     // this.dialogRef.close({ title: 'Hello World' });
