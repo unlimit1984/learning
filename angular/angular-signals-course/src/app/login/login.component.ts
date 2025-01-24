7b9015b16d0bf2164d835997f1383a591fde5ed4
unlimit1984 <unlimit_rmc@mail.ru>
2025-01-24 11:42:56 +0100

    [angular signals] 40 Add Login form

--- angular/angular-signals-course/src/app/login/login.component.ts
+++ angular/angular-signals-course/src/app/login/login.component.ts
@@ -1,20 +1,43 @@
-import {Component, inject} from '@angular/core';
-import {Router, RouterLink} from "@angular/router";
-import {AuthService} from "../services/auth.service";
-import {MessagesService} from "../messages/messages.service";
-import {FormBuilder, ReactiveFormsModule} from "@angular/forms";
+import { Component, inject } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
+import { AuthService } from '../services/auth.service';
+import { MessagesService } from '../messages/messages.service';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'login',
   standalone: true,
-  imports: [
-    RouterLink,
-    ReactiveFormsModule
-  ],
+  imports: [RouterLink, ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+  fb = inject(FormBuilder);
+
+  form = this.fb.group({
+    // email: ['', [Validators.required, Validators.email]],
+    // password: ['', Validators.required],
+    email: [''],
+    password: ['']
+  });
+
+  messageService = inject(MessagesService);
+
+  onLogin() {
+    try {
+      const { email, password } = this.form.value;
+      if (!email || !password) {
+        this.messageService.showMessage('Enter an email and password', 'error');
+      }
 
 
+
+    } catch (err) {
+      console.log(err);
+      this.messageService.showMessage(
+        'Login failed, please try again',
+        'error'
+      );
+    }
+  }
 }
