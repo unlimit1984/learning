5383a5f4b4bf11173270e93300201ebb851b9f57
unlimit1984 <unlimit_rmc@mail.ru>
2025-01-24 13:26:32 +0100

    [angular signals] 44 Save user profile in localStorage
    - Survive refresh

--- angular/angular-signals-course/src/app/services/auth.service.ts
+++ angular/angular-signals-course/src/app/services/auth.service.ts
@@ -20,6 +20,25 @@ export class AuthService {
 
   router = inject(Router);
 
+  constructor() {
+    this.loadUserFromStorage();
+
+    effect(() => {
+      const user = this.user();
+      if (user) {
+        localStorage.setItem(USER_STORAGE_KEY, JSON.stringify(user));
+      }
+    });
+  }
+
+  loadUserFromStorage() {
+    const json = localStorage.getItem(USER_STORAGE_KEY);
+    if (json) {
+      const user = JSON.parse(json);
+      this.#userSignal.set(user);
+    }
+  }
+
   async login(email: string, password: string): Promise<User> {
     const login$ = this.http.post<User>(`${environment.apiRoot}/login`, {
       email,
@@ -31,6 +50,7 @@ export class AuthService {
   }
 
   async logout() {
+    localStorage.removeItem(USER_STORAGE_KEY);
     this.#userSignal.set(null);
     await this.router.navigateByUrl('/login');
   }
