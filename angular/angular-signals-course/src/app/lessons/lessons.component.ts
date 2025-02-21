802bcc3c3bbb06553a15a6a3f941b29701535ee8
unlimit1984 <unlimit_rmc@mail.ru>
2025-02-21 15:02:00 +0100

    [angular signals] 54 Add Master Detail UI Pattern

--- angular/angular-signals-course/src/app/lessons/lessons.component.ts
+++ angular/angular-signals-course/src/app/lessons/lessons.component.ts
@@ -1,20 +1,25 @@
-import {Component, ElementRef, inject, signal, viewChild} from '@angular/core';
-import {LessonsService} from "../services/lessons.service";
-import {Lesson} from "../models/lesson.model";
-import {LessonDetailComponent} from "./lesson-detail/lesson-detail.component";
+import {
+  Component,
+  ElementRef,
+  inject,
+  signal,
+  viewChild
+} from '@angular/core';
+import { LessonsService } from '../services/lessons.service';
+import { Lesson } from '../models/lesson.model';
+import { LessonDetailComponent } from './lesson-detail/lesson-detail.component';
 
 @Component({
   selector: 'lessons',
   standalone: true,
-  imports: [
-    LessonDetailComponent
-  ],
+  imports: [LessonDetailComponent],
   templateUrl: './lessons.component.html',
   styleUrl: './lessons.component.scss'
 })
 export class LessonsComponent {
+  mode = signal<'master' | 'detail'>('master');
+  lessons = signal<Lesson[]>([]);
+  selectedLesson = signal<Lesson | null>(null);
 
-
-
-
+  lessonsService = inject(LessonsService);
 }
